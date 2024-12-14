7d53f5b25549473542ef096036d8cc4fad2eefc1
varshanegi18 <varshanegi066@gmail.com>
2024-12-14 12:53:35 +0530

    [echo builtin]

--- app/main.c
+++ app/main.c
@@ -3,6 +3,7 @@
 
 int main() {
     char input[100];
+    char i [100];
 
     while (1) {
         printf("$ ");
@@ -11,11 +12,11 @@ int main() {
         fgets(input, sizeof(input), stdin);
         input[strlen(input) - 1] = '\0'; // Remove newline character
         if(strcmp(input,"exit 0") ==0){
-       
         break;
         }
-        if (strcmp(input,"echo "input) ==0){
-        printf ("%s \n", input);
+        if (strcmp(input,"echo "i) ==0){
+        printf ("%s \n", i);
+        continue;
         }        
         printf("%s: command not found\n", input);
     }
